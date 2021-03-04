cdc9b6856388bf765ccd6040382c51fa461e10d5
Blake Yeboah <blakekyeboah@gmail.com>
2021-03-04 22:12:45 +1000

    Finished About Section

---
+++ components/Home/AboutSection.js
@@ -0,0 +1,35 @@
+import styles from '../../styles/AboutSection.module.css'
+
+const AboutSection = () => {
+
+    return (
+        <section className={styles.aboutSection}>
+
+            <h1 className={styles.title}>What is <span className={styles.primary}>Dev</span> Rocket?</h1>
+
+            <div className={styles.grid}>
+
+                <article className={styles.aboutSectionContent}>
+
+                    <p className={styles.aboutSectionParagraph}>DevRocket is a blog run by developer <a href="https://www.blakeyeboah.com/" target="_blank" className={styles.link}>Blake Yeboah</a> that has various posts related to web development. The blog was made to spread web development knowledge and help people from various competencies.</p>
+
+                </article>
+
+                <div className={styles.logoGridContainer}>
+
+                    <div className={styles.logoCircle}>
+
+                        <img className={styles.logo} alt="Dev Rocket Logo" src="/Logo.png" />
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+    )
+    
+}
+
+export default AboutSection;
