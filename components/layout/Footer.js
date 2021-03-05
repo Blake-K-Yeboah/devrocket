037f452c61c1724f0711b4bc6b533dc85a4c87fb
Blake Yeboah <blakekyeboah@gmail.com>
2021-03-05 19:57:43 +1000

    Finished Footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,3 +1,7 @@
+// Import Link From Next
+import Link from 'next/link';
+
+// Import Styles
 import styles from '../../styles/Footer.module.css';
 
 const Footer = () => {
@@ -5,7 +9,83 @@ const Footer = () => {
         <footer className={styles.footer}>
             
             <section className={styles.grid}>
-        .
+
+                <div className={styles.levelUpSection}>
+
+                    <h3 className={styles.levelUpHeading}>Level Up Your Skills</h3>
+
+                    <p className={styles.levelUpText}>Get notified on <b>new posts</b> released to level up your web dev skills</p>
+
+                    <div className={styles.inputGroup}>
+
+                        <input type="email" className={styles.input} placeholder="Enter Your Email" />
+
+                        <button className={styles.inputBtn}>Join</button>
+
+                    </div>
+
+                </div>
+
+                <div className={styles.linksSection}>
+
+                    <div className={styles.linkListContainer}>
+
+                        <h4 className={styles.linkListHeading}>Categories</h4>
+
+                        <ul className={styles.linkList}>
+
+                            <li>
+                                <Link href="/">
+                                    <a className={styles.linkListLink}>Front End</a>
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link href="/">
+                                    <a className={styles.linkListLink}>Back End</a>
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link href="/">
+                                    <a className={styles.linkListLink}>React</a>
+                                </Link>
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                    <div className={styles.linkListContainer}>
+
+                        <h4 className={styles.linkListHeading}>Navigation</h4>
+
+                        <ul className={styles.linkList}>
+
+                            <li>
+                                <Link href="/">
+                                    <a className={styles.linkListLink}>Home</a>
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link href="/posts">
+                                    <a className={styles.linkListLink}>Posts</a>
+                                </Link>
+                            </li>
+
+                            <li>
+                                <Link href="/about">
+                                    <a className={styles.linkListLink}>About</a>
+                                </Link>
+                            </li>
+
+                        </ul>
+
+                    </div>
+
+                </div>
+
             </section>
 
             <div className={styles.copyrightSection}>
