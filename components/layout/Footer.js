5a98bd3c61e1be37fc5ca43f274b671112466a01
Blake Yeboah <blakekyeboah@gmail.com>
2021-03-05 18:10:04 +1000

    Created Footer Component

---
+++ components/layout/Footer.js
@@ -0,0 +1,21 @@
+import styles from '../../styles/Footer.module.css';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            
+            <section className={styles.grid}>
+        .
+            </section>
+
+            <div className={styles.copyrightSection}>
+
+                <p className={styles.copyright}>&copy; Copyright 2021 DevRocket. All Rights Reserved</p>
+
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer;
