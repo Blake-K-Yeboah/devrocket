553ca5d1a84767e6a166f35756770d8ddd0211cb
Blake Yeboah <blakekyeboah@gmail.com>
2021-03-07 14:45:04 +1000

    Created Navbar Component

---
+++ components/layout/Navbar.js
@@ -0,0 +1,57 @@
+// Import Styles
+import styles from '../../styles/Navbar.module.css';
+
+// Import Link From Next
+import Link from 'next/link';
+
+const Navbar = ({ page }) => {
+    return (
+        <nav className={styles.navbar}>
+
+            <h2 className={styles.brand}>
+
+                <img src="/Logo.png" alt="Dev Rocket Logo" className={styles.brandImg} />
+
+                DevRocket <span className={styles.thin}>Blog</span>
+            
+            </h2>
+
+            <ul className={styles.navList}>
+
+                <li className={styles.navListItem}>
+
+                    <Link href="/">
+                        <a className={page === "home" ? styles.navListLinkActive : styles.navListLink}>
+                            Home
+                        </a>
+                    </Link>
+
+                </li>
+
+                <li className={styles.navListItem}>
+
+                    <Link href="/posts">
+                        <a className={page === "posts" ? styles.navListLinkActive : styles.navListLink}>
+                            Posts
+                        </a>
+                    </Link>
+
+                </li>
+
+                <li className={styles.navListItem}>
+
+                    <Link href="/about">
+                        <a className={page === "about" ? styles.navListLinkActive : styles.navListLink}>
+                            About
+                        </a>
+                    </Link>
+
+                </li>
+
+            </ul>
+
+        </nav>
+    )
+}
+
+export default Navbar;
