264b837396cde975d53f7390468ab5511395a382
Blake Yeboah <blakekyeboah@gmail.com>
2021-03-09 20:26:33 +1000

    Finished Post Component

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,9 +1,23 @@
 import styles from '../../styles/Post.module.css';
 
-const Post = () => {
+const Post = ({ data, htmlString }) => {
     return (
         <div className={styles.post}>
+            
+            <h1 className={styles.postTitle}>{data.title}</h1>
+            
+            <ul className={styles.postTags}>
 
+                {data.tags.split(' ').map(tag => <li className={styles.postTag} key={tag}>{tag}</li>)}
+
+            </ul>
+
+            <p className={styles.authorParagraph}>
+                Written by <img className={styles.authorImg} src={`/authors/${data.authorImg}`} alt={data.author} /> <span className={styles.bold}>{data.author}</span> on <span className={styles.bold}>{data.writtenDate}</span>
+            </p>
+
+            <div className={styles.postContent} dangerouslySetInnerHTML={{ __html: htmlString }} />
+            
         </div>
     )
 }
