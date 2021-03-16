4fd3e747bcff93a3eabab0f7ee27fe29606f59be
Blake Yeboah <blakekyeboah@gmail.com>
2021-03-16 19:38:00 +1000

    Added Tags

--- components/Home/Post.js
+++ components/Home/Post.js
@@ -4,12 +4,23 @@ import styles from '../../styles/LatestPosts.module.css';
 export default function Post({ post }) {
     return (
         <div className={styles.post}>
+
             <img src={`/thumbnails/${post.data.image}`} className={styles.postImg} />
+
             <h2 className={styles.postTitle}>{post.data.title}</h2>
+
+            <div className={styles.tagList}>
+                {post.data.tags.split(' ').map(tag => {
+                    return <div className={styles.tag} key={tag}>{tag}</div>
+                })}
+            </div>
+
             <p className={styles.postDesc}>{post.data.description}</p>
+
             <Link href={`post/${post.fileName.replace(".md", "")}`}>
                 <a className={styles.postBtn}>View Post</a>
             </Link>
+
         </div>
     )
 }
